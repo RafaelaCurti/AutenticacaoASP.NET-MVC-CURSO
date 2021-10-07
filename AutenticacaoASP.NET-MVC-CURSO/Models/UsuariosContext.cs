759cf501e30a03b499ccdd45c974129e28c6f620
Rafaela Curti de Paula <rafaela.curti@tech.math.group>
2021-10-07 15:18:29 -0300

    Definição da String de Conexão econfigurando o DBContext

---
+++ AutenticacaoASP.NET-MVC-CURSO/Models/UsuariosContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace AutenticacaoASP.NET_MVC_CURSO.Models
+{
+    public class UsuariosContext : DbContext
+    {
+        public UsuariosContext():base("Usuarios")
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
