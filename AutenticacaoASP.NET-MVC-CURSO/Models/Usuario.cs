5ec3e477eee4e04f8a931c9c8256010eff2f068a
Rafaela Curti de Paula <rafaela.curti@tech.math.group>
2021-10-07 15:00:01 -0300

    Criação do Projeto - Criando o OWIN Startup Class e definindo o modelo da aplicação com DataAnnotations

---
+++ AutenticacaoASP.NET-MVC-CURSO/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutenticacaoASP.NET_MVC_CURSO.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        public Int16 Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public String Nome { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public String Login { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public String Senha { get; set; }
+    }
+}
---
+++ AutenticacaoASP.NET-MVC-CURSO/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(AutenticacaoASP.NET_MVC_CURSO.Startup))]
+
+namespace AutenticacaoASP.NET_MVC_CURSO
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Autenticacao/Login")
+            });
+           
+        }
+    }
+}
